e388a574cac2f8980b751a427f8752b91e6c5648
sajjad-mugdho <sajjadmugdho@yahoo.com>
2024-03-13 19:37:47 +0600

    [Done] blog post section created

---
+++ components/BlogPost/BlogPost.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import sal from "sal.js";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+
+import Reaction from "../Common/Reaction";
+import loading from "../../public/images/icons/loader-one.gif";
+import avatar from "../../public/images/team/avater.png";
+import { useAppContext } from "@/context/Context";
+
+const BlogGeneretor = () => {
+  const { messages, isLoading } = useAppContext();
+  useEffect(() => {
+    sal();
+
+    const cards = document.querySelectorAll(".bg-flashlight");
+
+    cards.forEach((bgflashlight) => {
+      bgflashlight.onmousemove = function (e) {
+        let x = e.pageX - bgflashlight.offsetLeft;
+        let y = e.pageY - bgflashlight.offsetTop;
+
+        bgflashlight.style.setProperty("--x", x + "px");
+        bgflashlight.style.setProperty("--y", y + "px");
+      };
+    });
+  }, [isLoading]);
+
+  const { data: session } = useSession();
+
+  return (
+    <>
+      {messages?.map((message, index) => (
+        <div
+          className="chat-box-list pt--30"
+          id="chatContainer"
+          data-sal="slide-up"
+          data-sal-duration="700"
+          data-sal-delay="100"
+          key={index}
+        >
+          {message.role === "user" && (
+            <div className="chat-box author-speech bg-flashlight">
+              <div className="inner">
+                <div className="chat-section">
+                  <div className="author">
+                    <Image
+                      className="w-100"
+                      width={40}
+                      height={40}
+                      src={session?.user?.image || avatar}
+                      alt="Author"
+                    />
+                  </div>
+                  <div className="chat-content">
+                    <h6 className="title">{session?.user.name || "user"}</h6>
+                    <p>{message.content}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {message.role === "assistant" && (
+            <div className="chat-box ai-speech bg-flashlight">
+              <div
+                className="inner top-flashlight leftside light-xl"
+                key={index}
+              >
+                <div className="chat-section">
+                  <div className="author">
+                    <Image
+                      className="w-100"
+                      src={avatar}
+                      width={40}
+                      height={40}
+                      alt="ChatenAI"
+                    />
+                  </div>
+                  <div className="chat-content">
+                    <h6 className="title">
+                      ChatenAI
+                      <span className="rainbow-badge-card">Bot</span>
+                    </h6>
+
+                    {message.content.split("\n").map((line, lineIndex) => (
+                      <p key={lineIndex}>{line}</p>
+                    ))}
+                    <Reaction />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {isLoading && (
+            <div className="chat-section generate-section">
+              <div className="author">
+                <Image
+                  src={loading}
+                  width={40}
+                  height={40}
+                  alt="Loader Images"
+                />
+              </div>
+              <div className="chat-content">
+                <h6 className="title color-text-off mb--0">
+                  Generating answers for you…
+                </h6>
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default BlogGeneretor;
---
+++ pages/blog-post-generate/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import PageHead from "../Head";
+import Context from "@/context/Context";
+import HeaderDashboard from "@/components/Header/HeaderDashboard";
+import PopupMobileMenu from "@/components/Header/PopupMobileMenu";
+import RightpanelDashboard from "@/components/Common/RightpanelDashboard";
+import LeftpanelDashboard from "@/components/Common/LeftpanelDashboard";
+import Modal from "@/components/Common/Modal";
+import StaticbarDashboardBlog from "@/components/Common/StaticbarDashboardBlog";
+import BlogGeneretor from "@/components/BlogPost/BlogPost";
+
+const TextGeneratorPage = () => {
+  return (
+    <>
+      <PageHead title="Text Generator" />
+
+      <main className="page-wrapper rbt-dashboard-page">
+        <Context>
+          <div className="rbt-panel-wrapper">
+            <HeaderDashboard display="" />
+            <PopupMobileMenu />
+            <LeftpanelDashboard />
+            <RightpanelDashboard />
+            <Modal />
+
+            <div className="rbt-main-content">
+              <div className="rbt-daynamic-page-content">
+                <div className="rbt-dashboard-content">
+                  <div className="content-page">
+                    <BlogGeneretor />
+                  </div>
+                  <StaticbarDashboardBlog />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Context>
+      </main>
+    </>
+  );
+};
+
+export default TextGeneratorPage;
